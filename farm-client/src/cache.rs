b9c943649ea2c5ac85eeed8961bb878e6e7cdfdd
socksrust <solsockseng@gmail.com>
2022-06-17 22:20:09 -0300

    Initial commit

---
+++ farm-client/src/cache.rs
@@ -0,0 +1,55 @@
+//! Farm Client's cache to reduce the load on RPC endpoint.
+
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
+
+pub const RELOAD_INTERVAL: Duration = Duration::from_secs(21600);
+
+#[derive(Clone)]
+pub struct Cache<T> {
+    pub data: HashMap<String, T>,
+    pub last_load: Instant,
+    pub counter: u32,
+}
+
+impl<T> Default for Cache<T> {
+    fn default() -> Self {
+        Self {
+            data: HashMap::<String, T>::new(),
+            last_load: Instant::now() - RELOAD_INTERVAL,
+            counter: 0,
+        }
+    }
+}
+
+impl<T> Cache<T> {
+    pub fn is_stale(&self) -> bool {
+        self.data.is_empty() || Instant::now() - self.last_load >= RELOAD_INTERVAL
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
+    pub fn is_updated(&self, counter: u32) -> bool {
+        counter != self.counter
+    }
+
+    pub fn set(&mut self, data: HashMap<String, T>, counter: u32) {
+        self.data = data;
+        self.last_load = Instant::now();
+        self.counter = counter;
+    }
+
+    pub fn reset(&mut self) {
+        self.data = HashMap::<String, T>::new();
+        self.last_load = Instant::now() - RELOAD_INTERVAL;
+        self.counter = 0;
+    }
+
+    pub fn mark_not_stale(&mut self) {
+        self.last_load = Instant::now();
+    }
+}
---
+++ generate_vaults.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import subprocess
+import json
+import sys
+
+
+def main():
+    vaults_out = open(sys.argv[1], 'w')
+    tokens_out = open(sys.argv[2], 'w')
+    vault_program = sys.argv[3]
+    protocol = sys.argv[4].upper()
+
+    p = subprocess.Popen('/Users/jabur/foton/solana-program-library/farms/target/release/solana-farm-client list-all farm',
+                         shell=True,
+                         stdout=subprocess.PIPE)
+    data = '['
+    for line in p.stdout.readlines():
+        if line[:4].decode("utf-8") != protocol + '.':
+            continue
+        farm = line.decode("utf-8").split(':')[0]
+        if len('VT.' + protocol + '.STC.' + farm[4:]) >= 32:
+            raise ValueError("Len exceeded " + farm)
+        p2 = subprocess.Popen('/Users/jabur/foton/solana-program-library/farms/target/release/solana-farm-ctrl generate Vault ' +
+                              vault_program + ' ' + protocol + '.STC.' +
+                              farm[4:] + ' VT.' + protocol + '.STC.' +
+                              farm[4:],
+                              shell=True,
+                              stdout=subprocess.PIPE)
+        for log in p2.stdout.readlines():
+            data += log.decode("utf-8").rstrip('\n')
+        if p2.wait() == 0:
+            data += ','
+
+    data = data[:-1]
+    data += ']'
+
+    parsed = json.loads(data)
+    vaults_out.write(
+        '{"name": "Solana Vaults List", "timestamp": "2021-03-03T19:57:21+0000", "vaults":['
+    )
+    tokens_out.write(
+        '{"name": "Solana Token List", "timestamp": "2021-03-03T19:57:21+0000", "tokens":['
+    )
+    first_token = True
+    first_vault = True
+    for obj in parsed:
+        if 'chainId' in obj:
+            if not first_token:
+                tokens_out.write(',\n')
+            else:
+                first_token = False
+            tokens_out.write(json.dumps(obj, indent=2, sort_keys=False))
+        else:
+            if not first_vault:
+                vaults_out.write(',\n')
+            else:
+                first_vault = False
+            vaults_out.write(json.dumps(obj, indent=2, sort_keys=False))
+
+    vaults_out.write(']}')
+    tokens_out.write(']}')
+    vaults_out.close()
+    tokens_out.close()
+
+
+if __name__ == '__main__':
+    main()
